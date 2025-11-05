d1c30a45dbed33f8f4c29f93d5f25270816aeb58
Smitha Rajesh Naik <smitha.rajesh.naik@gmail.com>
2025-11-05 15:30:09 +0530

    Initial commit

--- src/test/java/tests/PlayWrightTest.java
+++ src/test/java/tests/PlayWrightTest.java
@@ -5,10 +5,11 @@ import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
+
 public class PlayWrightTest {
 	 public static void main(String[] args) {
 		  Playwright playwright = Playwright.create();
-		 Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+		 Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
 		  Page page = browser.newPage();
 		  page.navigate("https://amdocs.com");
 		  System.out.println("Hello.. I am " + page.title());
